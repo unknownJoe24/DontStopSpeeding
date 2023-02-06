8659734ab55776a050fad37de4ff1a2bbf8f11f8
Nick3530078 <nboose35008@gmail.com>
2023-02-06 17:43:58 -0500

    'Final' Initial Camera Implementation
    Drifting has been implemented and the camera has been moved down to more accurately reflect the desired design.

--- Assets/Assets/Scripts/Controllers/CamController.cs
+++ Assets/Assets/Scripts/Controllers/CamController.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class CamController : MonoBehaviour
 {
-    private GameObject target;              // What will the camera focus on
-    private Vector3 offset;                 // Where is the camera's offset from `target`
-    private Vector3 rotation;               // What is the rotation of the camera
-    private bool specialMove;               // Will the camera exhibit special behavior
-    VehicleBehaviour.WheelVehicle carInfo;  // Where to get the information of the car
-    private float yMove;                    // How much will the camera rotate when the car drifts
-    float carSpeed;                         // How fast is the player moving
+    private GameObject target;                      // What will the camera focus on
+    private Vector3 offset;                         // Where is the camera's offset from `target`
+    private Vector3 baseRotation;                   // What is the rotation of the camera
+    private bool specialMove;                       // Will the camera exhibit special behavior
+    private VehicleBehaviour.WheelVehicle carInfo;  // Where to get the information of the car
+    private float minTurn;                          // The minimum angle that will cause a drift effect
+    private float yMove;                            // How much will the camera rotate when the car drifts
+    private float carSpeed;                         // How fast is the player moving
 
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
-        offset = new Vector3(0f, 20f, -20f);
-        rotation = new Vector3(30f, 0f, 0f);
+        offset = new Vector3(0f, 5f, -7f);
+        baseRotation = new Vector3(15f, 0f, 0f);
         transform.position = target.transform.position + offset;
-        transform.rotation = Quaternion.Euler(rotation);
+        transform.rotation = Quaternion.Euler(baseRotation);
 
         specialMove = true;
         carInfo = target.GetComponent<VehicleBehaviour.WheelVehicle>();
-        yMove = 15f;
+        minTurn = 15f;
+        yMove = 5f;
         carSpeed = carInfo.Speed;
     }
 
@@ -37,9 +39,8 @@ public class CamController : MonoBehaviour
 
     private void LateUpdate()
     {
-        Debug.Log(Mathf.Log(Mathf.Max(0.1f, carSpeed)));
-        //transform.position = target.transform.position + offset + new Vector3(0f, 0f, - Mathf.Log(Mathf.Max(0.1f, carSpeed)));
-        transform.position = target.transform.position + offset + new Vector3(0f, 0f, -Mathf.Min(carSpeed / 10f, 7.5f));
+        transform.position = target.transform.position + offset + new Vector3(0f, 0f, - Mathf.Log(Mathf.Max(0.1f, carSpeed)));
+        //transform.position = target.transform.position + offset + new Vector3(0f, 0f, -Mathf.Min(carSpeed / 10f, 2.5f));
     }
 
     // Rotates the yaw of the camera by yMove when the player is turning
@@ -47,18 +48,27 @@ public class CamController : MonoBehaviour
     {
         if (specialMove)
         {
-            float turn = Input.GetAxis("Horizontal");
-            if (turn < 0f)  // Left
+            bool drift = Input.GetButton("Drift");
+            float angle = target.transform.rotation.eulerAngles.y;
+
+            if (drift)
             {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rotation + new Vector3(0f, -yMove, 0f)), .1f);
-            }
-            else if (turn > 0f) // Right
-            {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rotation + new Vector3(0f, yMove, 0f)), .1f);
+                if (angle > 180f + minTurn && angle < 360f - minTurn)  // Left
+                {
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(baseRotation + new Vector3(0f, -yMove, 0f)), .1f);
+                }
+                else if (angle > minTurn && angle < 180f - minTurn) // Right
+                {
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(baseRotation + new Vector3(0f, yMove, 0f)), .1f);
+                }
+                else
+                {
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(baseRotation), .1f);
+                }
             }
             else
             {
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rotation), .1f);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(baseRotation), .1f);
             }
         }
     }
