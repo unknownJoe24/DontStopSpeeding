6c2a5cd3b76df691de64d193a4365f240a3cd301
David Russell <davidallenr@gmail.com>
2023-02-10 08:43:04 -0500

    Added some icon assets colored bar

--- Assets/Assets/Scripts/Utility/GasSystem.cs
+++ Assets/Assets/Scripts/Utility/GasSystem.cs
@@ -1,14 +1,16 @@
-using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GasSystem : MonoBehaviour
 {
     public float gasLevel = 15.0f;
-    public float decreaseRate = 1.0f; // 1 gallon per 20 seconds
+    public float decreaseRate = 20.0f; // 1 gallon per 20 seconds
     public float maxGasLevel = 20.0f;
     public float purchaseAmount = 5.0f;
     private float lastGasUpdateTime = 0.0f;
     public bool inputDisabled = false;
+    public Slider slider;
+    public Image fillBar;
     private void Update()
     {
         if (inputDisabled)
@@ -20,10 +22,12 @@ public class GasSystem : MonoBehaviour
         {
             gasLevel -= 1.0f;
             lastGasUpdateTime = Time.time;
-            Debug.Log("Gas level: " + gasLevel);
+            SetGasValue(gasLevel);
 
             if (gasLevel <= 0.0f)
             {
+                gasLevel = 0.0f;
+                SetGasValue(gasLevel);
                 inputDisabled = true;
                 Debug.Log("You lose");
             }
@@ -33,7 +37,31 @@ public class GasSystem : MonoBehaviour
         {
             gasLevel += purchaseAmount;
             gasLevel = Mathf.Clamp(gasLevel, 0.0f, maxGasLevel);
+            SetGasValue(gasLevel);
             Debug.Log("Purchased gas. Gas level: " + gasLevel);
         }
     }
+
+    public void SetGasValue(float amount)
+    {
+        slider.value = amount;
+        SetGasBarColor(slider.value);
+    }
+
+    private void SetGasBarColor(float value)
+    {
+        switch (value)
+        {
+            case (> 5) when value <= 10:
+                fillBar.color = new Color(255, 255, 0, 100);
+                break;
+            case (<= 5):
+                fillBar.color = new Color(255, 0, 0, 100);
+                break;
+            default:
+                fillBar.color = new Color(0, 255, 0, 100);
+                break;
+        }
+    }
+
 }
