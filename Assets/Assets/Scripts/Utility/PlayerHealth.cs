be04ce94dc6b836d0469bba01c8b6a3c22a4b9fb
Sarah Reen <102923707+TofuKiller9000@users.noreply.github.com>
2023-04-23 17:47:11 -0400

    Updated PlayerHealth Script to avoid conflict
    Updated PlayerHealth script to include new code from main for invulnerability and damage while also including UI implementation for health script

--- Assets/Assets/Scripts/Utility/PlayerHealth.cs
+++ Assets/Assets/Scripts/Utility/PlayerHealth.cs
@@ -8,51 +8,93 @@ public class PlayerHealth : MonoBehaviour
 {
 
     public float health;                                // current player health
-    public bool defense = false;
-    public bool regen = false;
+    public bool dead;                                   // is the player dead
+    public float invulnerableDur;                       // how long the player becomes invulnerable
+    public bool defense = false;                        // defense - redundant?
+    public bool regen = false;                          // does the car regenerate
     public float regenRate;
+
+    public AudioClip hurtSound;                         // sound when the player is hurt
+    public AudioClip deathSound;                        // sound when the player dies
 
     float currentSpeed;                                 // current speed player is going
 
-    private float second;
-    private VehicleBehaviour.WheelVehicle playerInfo;   // Reference to the WheelVehicle script
+
+    private float invulnerableTime;                     // the time since invulnerability began
+    private float second;                               // the time since health was regenerated
+    private LaneSwitcher playerInfo;                    
+    //private VehicleBehaviour.WheelVehicle playerInfo;   // Reference to the WheelVehicle script
     private Rigidbody playerRigidBody;
     public TMP_Text healthText;
     public Image healthBar;
 
+
+    GameObject[] colliderObjects;                       // the individual colliders of the player
+
     // Start is called before the first frame update
     void Start()
     {
-        health = 100;                                    // sets player health at start
-        regenRate = 5.0f;
+
+        // member initialization
+        invulnerableDur = 2f;
+        regenRate = 1f;
+        invulnerableTime = 0f;
         second = 0.0f;
 
-        playerInfo = GameObject.FindGameObjectWithTag("Player").GetComponent<VehicleBehaviour.WheelVehicle>();
-        playerRigidBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        //health = 100;                                    // sets player health at start
+
+
+        playerInfo = gameObject.GetComponent<LaneSwitcher>();
+        playerRigidBody = gameObject.GetComponent<Rigidbody>();
         //healthText = GameObject.FindGameObjectWithTag("Health").GetComponent<TMP_Text>();
+
+        // get all objects under gameObject that have a collider
+        // this is used to swap the player's layer
+        Collider[] allColliders = GetComponentsInChildren<Collider>();
+        colliderObjects = new GameObject[allColliders.Length];
+        if (allColliders.Length == 0)
+            Debug.LogError("The colliders were not achieved properly");
+
+        for (int i = 0; i < allColliders.Length; ++i)
+        {
+            colliderObjects[i] = allColliders[i].gameObject;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //currentSpeed = playerInfo.Speed; -- THIS LINE DOESN'T WORK
+        currentSpeed = playerInfo.Speed; //-- THIS LINE DOESN'T WORK(?)
 
+        // redisplay the health HUD
         redisplayInfo();
 
-        if(health > 0)
+        // handle invulnerability
+        if (gameObject.layer == LayerMask.NameToLayer("Invincibility"))
+        {
+            invulnerableTime += Time.deltaTime;
+            // make the player vulnerable again
+            if (invulnerableTime >= invulnerableDur)
+            {
+                handleInvulnerability(false);
+            }
+        }
+
+        // recover health if the player has taken damage
+        if (regen && health > 0)
+            recoverHealth();
+
+        if (health > 0)
         {
                 recoverHealth();
         }
 
-        if(health <= 0){
-            //die
-            print("ded");
-            playerInfo.IsPlayer = false;
-            playerInfo.Throttle = 0f;
-            playerRigidBody.velocity = new Vector3(0f, 0f, 0f);
-        }
+        // kill the player if they ran out of health
+        if (!dead && health <= 0)
+            killPlayer();
 
-        if(Input.GetKeyDown("k"))       // this is temporary
+        if (Input.GetKeyDown("k"))       // this is temporary
         {
             takeDamage();
             //print("taking damage");
@@ -62,16 +104,16 @@ public class PlayerHealth : MonoBehaviour
     // Recover player health over time
     void recoverHealth()
     {
-        // recover 5 health every 10 seconds
-        // get time and then check every 10 seconds
-        if(health < 100 && regen)
+        // recover 5 health every regenRate seconds
+        // get time and then check every regenRate seconds
+        if (health < 100 && regen)
         {
             second += Time.deltaTime;
-            if(second >= 5.0)
+            if (second >= regenRate)
             {
                 health += 5;
                 second = 0;
-                if(health > 100)
+                if (health > 100)
                 {
                     health = 100;
                 }
@@ -82,21 +124,29 @@ public class PlayerHealth : MonoBehaviour
     // Remove health from player when damage is dealt
     void takeDamage()
     {
+        if (currentSpeed > 0)
+        {
+            health -= currentSpeed / 10;
+            if (health > 0)
+                SoundManager.Instance.Play(hurtSound, 1f);
+
+            handleInvulnerability(true);
+        }
+        /*
             if(currentSpeed > 0)
             {
             if(defense)
             {
                 float tmpDamage = currentSpeed / 10;
                 health -= tmpDamage / 2;
-                print("defense damage taken");
             }
             if(!defense)
             {
                 //Current Speed before impact / 10 = damage dealt
                 health -= currentSpeed / 10;
-                print("taking damage");
             }
-        }
+        
+        }*/
     }
 
     void redisplayInfo()
@@ -105,12 +155,81 @@ public class PlayerHealth : MonoBehaviour
         healthBar.fillAmount = health / 100;
     }
 
-    void OnTriggerEnter (Collider other)
+    // kills the player, prevents movement, and plays the death animation
+    public void killPlayer()
     {
-        if(other.tag == "testDamage")
+        // stop movement
+        playerInfo.DisableMovement = true;
+        playerRigidBody.velocity = new Vector3(0f, 0f, 0f);
+
+        // create a new object at the location of the car to hold the car animation
+        GameObject emptyObj = new GameObject();
+        GameObject emptyInst = Instantiate(emptyObj);
+
+        emptyInst.transform.position = transform.position;
+        gameObject.SetActive(false);
+
+        dead = true;
+
+        // create a new car to be animated
+        GameObject newCar = Instantiate(gameObject);
+        //newCar.gameObject.GetComponent<PlayerHealth>().dead = true;
+        newCar.transform.parent = emptyInst.transform;
+        Camera.main.gameObject.GetComponent<CamController>().updateTarget(newCar);
+        newCar.SetActive(true);
+
+        // set the position and launch upwards
+        newCar.transform.position = emptyInst.transform.position;
+        Rigidbody newBody = newCar.GetComponent<Rigidbody>();
+
+        //newBody.constraints = RigidbodyConstraints.FreezeRotation;
+        newBody.constraints = RigidbodyConstraints.FreezePositionX;
+        //newBody.constraints = RigidbodyConstraints.FreezePosition;
+        //newBody.AddForce(Vector3.up * 50f);
+
+        // enable the animator and play the animation
+        Animator playerAnimator = newCar.GetComponent<Animator>();
+        playerAnimator.updateMode = AnimatorUpdateMode.AnimatePhysics;
+        playerAnimator.enabled = true;
+        playerAnimator.bodyPosition = transform.position;
+        playerAnimator.rootPosition = transform.position;
+        playerAnimator.applyRootMotion = true;
+        playerAnimator.SetBool("Dead", true);
+
+        SoundManager.Instance.Play(deathSound, 1f);
+    }
+
+    // change the player's vulnerability
+    void handleInvulnerability(bool _status)
+    {
+        if (_status)
+        {
+            //set the layer for every object under and including the player that has a collider
+            gameObject.layer = LayerMask.NameToLayer("Invincibility");
+            for (int i = 0; i < colliderObjects.Length; ++i)
+            {
+                colliderObjects[i].layer = LayerMask.NameToLayer("Invincibility");
+            }
+        }
+        else
+        {
+            //set the layer for every object under and including the player that has a collider
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            for (int i = 0; i < colliderObjects.Length; ++i)
+            {
+                colliderObjects[i].layer = LayerMask.NameToLayer("Default");
+            }
+            invulnerableTime = 0f;
+        }
+    }
+
+    // checks for collisions with the player (only the collider for the super object (I believe))
+    private void OnCollisionEnter(Collision collision)
+    {
+        // only take damage if the player hits an obstacle
+        if (LayerMask.LayerToName(collision.gameObject.layer) == "Damage")
         {
             takeDamage();
-            print("player collided with object");
         }
     }
 }
